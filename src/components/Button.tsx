4fa15d4588104f7af4189113c66c309d176e4f64
Afeez G Lawal <afeezlg@gmail.com>
2023-08-19 00:30:45 +0100

    chore: add button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,40 @@
+import { FunctionComponent, h } from 'preact';
+
+interface ButtonProps {
+  text: string;
+  color: string;
+  size?: 'md' | 'sm' | 'full';
+  onClick?: () => void;
+}
+
+export const Button: FunctionComponent<ButtonProps> = ({
+  text,
+  color,
+  onClick,
+  size = 'md',
+}) => {
+  const onClickHandler = () => {
+    if (onClick) {
+      onClick();
+    }
+  };
+  let widthClasses = 'w-auto';
+
+  if (size === 'md') {
+    widthClasses = 'w-32';
+  } else if (size === 'sm') {
+    widthClasses = 'w-fit';
+  } else if (size === 'full') {
+    widthClasses = 'w-full';
+  }
+
+  return (
+    <button
+      onClick={onClickHandler}
+      className={`text-white font-semibold py-2 px-4 rounded-xl ${widthClasses}`}
+      style={{ backgroundColor: color }}
+    >
+      {text}
+    </button>
+  );
+};
