b134346f05fe3bf955dc6e2d11ed56e54ec505c3
Afeez G Lawal <afeezlg@gmail.com>
2023-08-18 17:18:08 +0100

    style: update wrapper styles for survey placement

--- src/surveys/App.tsx
+++ src/surveys/App.tsx
@@ -1,19 +1,29 @@
 import { VNode, h } from 'preact';
 import { useState } from 'preact/hooks';
 
-import { ID, Survey, SurveyAnswer } from '../types';
 import { Wrapper } from '../components';
+import { ID, Survey, SurveyAnswer } from '../types';
 import SurveyView from './components/Survey';
 
 interface AppProps {
   survey: Survey;
   onSurveyDisplayed?: (surveyId: ID) => void;
   onSurveyClosed?: (surveyId: ID) => Promise<void>;
-  onQuestionAnswered?: (surveyId: ID, questionId: ID, answer: SurveyAnswer) => void;
+  onQuestionAnswered?: (
+    surveyId: ID,
+    questionId: ID,
+    answer: SurveyAnswer
+  ) => void;
   onSurveyCompleted?: (surveyId: ID) => void;
 }
 
-export default function App({ survey, onSurveyDisplayed, onSurveyClosed, onQuestionAnswered, onSurveyCompleted }: AppProps): VNode {
+export default function App({
+  survey,
+  onSurveyDisplayed,
+  onSurveyClosed,
+  onQuestionAnswered,
+  onSurveyCompleted,
+}: AppProps): VNode {
   const [isOpen, setIsOpen] = useState(true);
 
   const close = () => {
@@ -24,11 +34,11 @@ export default function App({ survey, onSurveyDisplayed, onSurveyClosed, onQuest
   };
 
   return (
-    <div id="fbjs">
+    <div id='fbjs'>
       <Wrapper
         // isOpen={isOpen}
-        // close={close}
-        // placement={survey.settings.placement}
+        close={close}
+        placement={survey.settings.placement}
       >
         <SurveyView
           survey={survey}
