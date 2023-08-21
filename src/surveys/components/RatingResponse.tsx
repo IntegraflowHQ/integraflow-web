57192a94522dde7af0fd04002f382fbd11e65102
Afeez G Lawal <afeezlg@gmail.com>
2023-08-21 22:03:15 +0100

    chore: get range content from options or count

--- src/surveys/components/RatingResponse.tsx
+++ src/surveys/components/RatingResponse.tsx
@@ -1,22 +1,40 @@
 import { h } from 'preact';
 import { useEffect, useState } from 'preact/hooks';
 import { Header } from '../../components';
-import { ID, Question, RangeSettings, Theme } from '../../types';
+import {
+  AnswerType,
+  ID,
+  Question,
+  RangeSettings,
+  SurveyAnswer,
+  Theme,
+} from '../../types';
 import RatingIcon from './RatingIcon';
 
 interface RatingResponseProps {
   question: Question;
-  onAnswered: (questionId: ID, value: string) => void;
+  onAnswered: (questionId: ID, answer: SurveyAnswer) => void;
   theme?: Theme;
 }
 
 function RatingResponse({ question, onAnswered, theme }: RatingResponseProps) {
-  const [answer, setAnswer] = useState(0);
+  const [rating, setRating] = useState(0);
+  const [answerId, setAnswerId] = useState<ID | null>(null);
 
   useEffect(() => {
     const answerQuestion = () => {
-      if (answer === 0) return;
-      onAnswered(question.id, answer.toString());
+      if (rating === 0) return;
+
+      if (answerId)
+        return onAnswered(question.id, {
+          type: AnswerType.RATING,
+          id: answerId,
+        });
+
+      onAnswered(question.id, {
+        type: AnswerType.RATING,
+        value: rating.toString(),
+      });
     };
 
     const timeoutId = setTimeout(answerQuestion, 500);
@@ -24,7 +42,48 @@ function RatingResponse({ question, onAnswered, theme }: RatingResponseProps) {
     return () => {
       clearTimeout(timeoutId);
     };
-  }, [answer]);
+  }, [rating, answerId]);
+
+  let rangeContent = null;
+
+  if (question.options && question.options.length > 0) {
+    rangeContent = question.options
+      .sort((a, b) => a.orderNumber - b.orderNumber)
+      .map((option) => (
+        <button
+          onClick={() => {
+            setAnswerId(option.id);
+            setRating(option.orderNumber);
+          }}
+          key={option.label}
+        >
+          {
+            <RatingIcon
+              shape={(question.settings as RangeSettings).shape}
+              color={
+                option.orderNumber <= rating
+                  ? '#FFCB45'
+                  : theme?.answer ?? '#F2F2F2'
+              }
+            />
+          }
+        </button>
+      ));
+  } else if ((question.settings as RangeSettings).count) {
+    rangeContent = Array.from(
+      { length: (question.settings as RangeSettings).count! },
+      (_, index) => index + 1
+    ).map((num) => (
+      <button onClick={() => setRating(num)} key={num.toString()}>
+        {
+          <RatingIcon
+            shape={(question.settings as RangeSettings).shape}
+            color={num <= rating ? '#FFCB45' : theme?.answer ?? '#F2F2F2'}
+          />
+        }
+      </button>
+    ));
+  }
 
   return (
     <div className={'px-6 space-y-3'}>
@@ -35,18 +94,7 @@ function RatingResponse({ question, onAnswered, theme }: RatingResponseProps) {
         centered
       />
 
-      <div className='flex justify-center gap-6 px-6'>
-        {[1, 2, 3, 4, 5].map((num) => (
-          <button onClick={() => setAnswer(num)} key={num.toString()}>
-            {
-              <RatingIcon
-                shape={(question.settings as RangeSettings).shape}
-                color={num <= answer ? '#FFCB45' : theme?.answer ?? '#F2F2F2'}
-              />
-            }
-          </button>
-        ))}
-      </div>
+      <div className='flex justify-center gap-6 px-6'>{rangeContent}</div>
 
       <div className='flex justify-between'>
         <span style={{ color: theme?.question ?? '#050505' }}>
