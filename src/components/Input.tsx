04fdcccd265248041236e9403d913ab0291aa096
Adebanjo Afolasope <folasopeadebanjo@gmail.com>
2023-08-31 20:22:39 +0100

    add input component

---
+++ src/components/Input.tsx
@@ -0,0 +1,72 @@
+import { h } from 'preact';
+import { FormField, FormFieldType, ID } from '../types';
+import { hexToRgba } from '../utils';
+import { JSXInternal } from 'preact/src/jsx';
+
+type Props = {
+  option: FormField;
+  label?: string;
+  color?: string;
+  name: string;
+  value: string;
+  id: ID;
+  type: FormFieldType;
+  placeholder?: string;
+  required?: boolean;
+  onChange: (value: string) => void;
+};
+export const Input = ({
+  option,
+  label,
+  color = '#050505',
+  name,
+  id,
+  placeholder,
+  value,
+  required,
+  onChange,
+}: Props) => {
+  let inputType: JSXInternal.HTMLAttributes<
+    HTMLInputElement
+  >['type'] = undefined;
+  switch (option.type) {
+    case FormFieldType.EMAIL:
+      inputType = 'email';
+      break;
+    case FormFieldType.WEBSITE:
+      inputType = 'url';
+      break;
+    default:
+      inputType = 'text';
+      break;
+  }
+
+  return (
+    <div>
+      <label
+        style={{
+          color: color,
+        }}
+        className={'block mb-1'}
+        htmlFor={id + ''}
+      >
+        {label}
+        {required ? <span>{'*'}</span> : null}
+      </label>
+      <input
+        type={inputType}
+        className={'w-full border rounded-md p-2'}
+        style={{
+          color: color,
+          backgroundColor: hexToRgba(color, 0.1),
+        }}
+        required={required}
+        name={name}
+        id={id + ''}
+        placeholder={placeholder}
+        value={value}
+        onInput={(e) => onChange(e.currentTarget.value)}
+      />
+    </div>
+  );
+};
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,7 @@
+import { Input } from './Input';
+
 export { Button } from './Button/Button';
 export { default as DatePicker } from './DatePicker';
 export * from './Header';
 export * from './Wrapper';
+export { Input } from './Input';
